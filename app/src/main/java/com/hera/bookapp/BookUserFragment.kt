0b7034ad4bf27e6117b30ca7b8ee99c3b67880a7
yusufcan <yusufcakmak29@gmail.com>
2023-03-19 23:27:39 +0300

    pdfuseractivity is finished

--- app/src/main/java/com/hera/bookapp/BookUserFragment.kt
+++ app/src/main/java/com/hera/bookapp/BookUserFragment.kt
@@ -1,10 +1,18 @@
 package com.hera.bookapp
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.hera.bookapp.databinding.FragmentBookUserBinding
 
 
@@ -40,14 +48,135 @@ public companion object{
         if (args!=null){
             categoryId=args.getString("categoryId")!!
             category=args.getString("category")!!
-            uid=args.getString(uid)!!
+            uid=args.getString("uid")!!
         }
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         binding= FragmentBookUserBinding.inflate(LayoutInflater.from(context),container,false)
+        //load pdf according to category, this fragment will have new instance to load each category pdfs
+        Log.d(TAG,"onCreateView:Category:$category")
+        if (category=="All"){
+            //load all books
+            loadAllBooks()
+        }
+        else if(category=="Most Viewed"){
+
+            //load most viewed books
+            loadMostViewedDownloadedBooks("viewsCount")
+
+        }
+        else if (category=="Most Downloaded"){
+            loadMostViewedDownloadedBooks("downloadsCount")
+
+        }
+        else{
+            //load selected category books
+            loadCategoryizedBooks()
+        }
+        //search
+        binding.searchEt.addTextChangedListener {
+            object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    try {
+                        adapterPdfUser.filter.filter(s)
+
+                    }catch (e:Exception){
+                        Log.d(TAG,"onTextChange:SEARCH EXCEP. :${e.message}")
+                    }
+
+                }
+
+                override fun afterTextChanged(p0: Editable?) {
+
+                }
+
+
+             }}
+
+
+
         return binding.root
     }
 
+    private fun loadAllBooks() {
+       pdfArrayList= ArrayList()
+        val ref=FirebaseDatabase.getInstance().getReference("Books")
+        ref.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                pdfArrayList.clear()
+                for (ds in snapshot.children){
+                    //get data
+                    val model =ds.getValue(ModelPdf::class.java)
+                    //add to list
+                    pdfArrayList.add(model!!)
+                }
+                //setup adapter
+                adapterPdfUser = AdapterPdfUser(pdfArrayList, filterList = pdfArrayList)
+                //set adapter to rv
+                binding.bookRv.adapter=adapterPdfUser
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    private fun loadMostViewedDownloadedBooks(orderBy: String) {
+        pdfArrayList= ArrayList()
+        val ref=FirebaseDatabase.getInstance().getReference("Books")
+        ref.orderByChild(orderBy).limitToLast(10)//load most viewed or most mownloaded books. order
+            .addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                pdfArrayList.clear()
+                for (ds in snapshot.children){
+                    //get data
+                    val model =ds.getValue(ModelPdf::class.java)
+                    //add to list
+                    pdfArrayList.add(model!!)
+                }
+                //setup adapter
+                adapterPdfUser =AdapterPdfUser(pdfArrayList,pdfArrayList)
+                //set adapter to rv
+                binding.bookRv.adapter=adapterPdfUser
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
 
     }
+
+    private fun loadCategoryizedBooks() {
+        pdfArrayList= ArrayList()
+        val ref=FirebaseDatabase.getInstance().getReference("Books")
+        ref.orderByChild("categoryId").equalTo(categoryId)
+            .addValueEventListener(object :ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    pdfArrayList.clear()
+                    for (ds in snapshot.children){
+                        //get data
+                        val model =ds.getValue(ModelPdf::class.java)
+                        //add to list
+                        pdfArrayList.add(model!!)
+                    }
+                    //setup adapter
+                    adapterPdfUser =AdapterPdfUser(pdfArrayList,pdfArrayList)
+                    //set adapter to rv
+                    binding.bookRv.adapter=adapterPdfUser
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+            })
+    }
+}
--- app/src/main/java/com/hera/bookapp/DashBoardUserActivity.kt
+++ app/src/main/java/com/hera/bookapp/DashBoardUserActivity.kt
@@ -1,40 +1,166 @@
 package com.hera.bookapp
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.appbar.AppBarLayout.Behavior
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.hera.bookapp.databinding.ActivityDashBoardBinding
 
 class DashBoardUserActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDashBoardBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var categoryArrayList: ArrayList<ModelCategory>
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityDashBoardBinding.inflate(layoutInflater)
+        binding = ActivityDashBoardBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
         checkUser()
+        setupWithPagerAdapter(binding.viewPager)
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
 
         binding.lagoutBtn.setOnClickListener {
             firebaseAuth.signOut()
-            startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+    }
+
+    private fun setupWithPagerAdapter(viewPager: ViewPager) {
+        viewPagerAdapter = ViewPagerAdapter(
+            supportFragmentManager,
+            FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT,
+            this
+        )
+        categoryArrayList = ArrayList()
+        //load categories from db
+        val ref = FirebaseDatabase.getInstance().getReference("Category")
+        //gbt
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //clear list
+                categoryArrayList.clear()
+                //Load some static categories e.g all, most viewed most downloaed
+                //add data to models
+                val modelAll = ModelCategory("01", "All", 1, "")
+                val modelMostViewed = ModelCategory("01", "Most Viewed", 1, "")
+                val modelMostDownloaded = ModelCategory("01", "Most Downloaded", 1, "")
+                //add to list
+                categoryArrayList.add(modelAll)
+                categoryArrayList.add(modelMostViewed)
+                categoryArrayList.add(modelMostDownloaded)
+                viewPagerAdapter.addFragment(
+                    BookUserFragment.newInstance(
+                        "${modelAll.id}",
+                        "${modelAll.category}",
+                        "${modelAll.uid}"
+                    ), modelAll.category
+                )
+                viewPagerAdapter.addFragment(
+                    BookUserFragment.newInstance(
+                        "${modelMostViewed.id}",
+                        "${modelMostViewed.category}",
+                        "${modelMostViewed.uid}"
+                    ), modelMostViewed.category
+                )
+                viewPagerAdapter.addFragment(
+                    BookUserFragment.newInstance(
+                        "${modelMostDownloaded.id}",
+                        "${modelMostDownloaded.category}",
+                        "${modelMostDownloaded.uid}"
+                    ), modelMostDownloaded.category
+                )
+                //reflesh list
+                viewPagerAdapter.notifyDataSetChanged()
+                //load from firebas db
+                for (ds in snapshot.children){
+                    //get data in model
+                    val model =ds.getValue(ModelCategory::class.java)
+                    //add to list
+                    categoryArrayList.add(model!!)
+                    //add to viewPagerAdapter
+                    viewPagerAdapter.addFragment(
+                        BookUserFragment.newInstance(
+                            "${model.id}",
+                            "${model.category}",
+                            "${model.uid}"
+                        ), model.category
+                    )
+                    //reflesh list
+                    viewPagerAdapter.notifyDataSetChanged()
+
+                }
+            }
+
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+
+        //setup adapter to viewpager
+        viewPager.adapter=viewPagerAdapter
+    }
+
+    class ViewPagerAdapter(fm: FragmentManager, behavior: Int, context: Context) :
+        FragmentPagerAdapter(fm, behavior) {
+        //holds list of fragments i.e new instances of same fragment for each category
+        private val fragmentList: ArrayList<BookUserFragment> = ArrayList()
+
+        //list of titles of categories, for tabs
+        private val fragmentTitleList: ArrayList<String> = ArrayList()
+        private val context: Context
+
+        init {
+            this.context = context
+
+        }
+
+        override fun getCount(): Int {
+            return fragmentList.size
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return fragmentList[position]
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return fragmentTitleList[position]
+        }
+
+        public fun addFragment(fragment: BookUserFragment, title: String) {
+            //add fragment that will be passed as parameter in fragmentList
+            fragmentList.add(fragment)
+            //add title that will be passed as parameter
+            fragmentTitleList.add(title)
+        }
+
     }
 
     private fun checkUser() {
         // get current user
         val firebaseuser = firebaseAuth.currentUser
-        if (firebaseuser==null){
+        if (firebaseuser == null) {
             // not logged in, user can stay in user dashboarf without login
-            binding.subTitleTv.text="Not Logged In"
+            binding.subTitleTv.text = "Not Logged In"
 
 
-        }else{
-            val email=firebaseuser.email
-            binding.subTitleTv.text=email
+        } else {
+            val email = firebaseuser.email
+            binding.subTitleTv.text = email
 
         }
     }
